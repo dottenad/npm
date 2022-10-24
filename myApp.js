131c33465d5a7b1f30be3f94c62ea1eb3229d098
dottenad <ottenadd@gmail.com>
2022-10-24 13:48:05 -0700

    commit

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-const absolutePath = __dirname + /views/index.html;
+const absolutePath = __dirname + "/views/index.html";
 
 app.get("/", function (req, res) {
     res.sendFile(absolutePath);
